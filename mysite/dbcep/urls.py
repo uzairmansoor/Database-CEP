aebad852be2e87463ef7fb34d56d042c603f1f40
Uzair Mansoor <uzairmansoor123123@gmail.com>
2022-05-13 23:22:44 +0500

    final

--- mysite/dbcep/urls.py
+++ mysite/dbcep/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('menu/',views.menu,name='menu'),
     path('gallery/',views.gallery,name='gallery'),
     path('core_body/',views.gallery,name='core_body'),
+    path('signin/',views.signin,name='signin'),
+    path('signup/',views.signup,name='signup')
 ]
--- mysite/dbcep/views.py
+++ mysite/dbcep/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import *
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 
 # Create your views here.
 
@@ -25,3 +28,51 @@ def gallery(request):
 
 def menu(request):
     return render(request,'dbcep/menu.html')
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'signup.html', {'form': form})
+    else:
+        form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
+
+def signin(request):
+    if request.user.is_authenticated:
+        return render(request, 'homepage.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            form = AuthenticationForm(request.POST)
+            return render(request, 'signin.html', {'form': form})
+    else:
+        form = AuthenticationForm()
+        return render(request, 'signin.html', {'form': form})
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+
+def signout(request):
+    logout(request)
+    return redirect('/')
+
+    
